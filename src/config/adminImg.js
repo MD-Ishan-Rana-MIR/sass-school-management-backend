26ceace8f842d8a7779ca16b7b40b2c9d201663a
ishanrana363 <ishanrana363@gmail.com>
2026-01-07 11:12:53 +0600

    admin profile completed d

---
+++ src/config/adminImg.js
@@ -0,0 +1,38 @@
+const multer = require("multer");
+const path = require("path");
+
+const uploadPath = path.join(__dirname, "../uploads/admins");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadPath);
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const uniqueName = `admins-${Date.now()}${ext}`;
+    cb(null, uniqueName);
+  },
+});
+
+// File filter
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /jpeg|jpg|png|webp/;
+  const ext = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+  const mime = allowedTypes.test(file.mimetype);
+
+  if (ext && mime) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only images are allowed"));
+  }
+};
+
+const adminImgUpload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 2MB
+  },
+});
+
+module.exports = adminImgUpload;
--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,8 +1,41 @@
 const adminRoute = require("express").Router();
 
-const { createAdmin } = require("../controllers/admin/admin.auth.controller");
-const role = require("../middlewares/role.middleware");
+const adminOuth = require("../middlewares/admin.role.middleware");
 
-adminRoute.post("/create-admin", role("super-admin"), createAdmin);
+const adminImgUpload = require("../config/adminImg");
+
+const {
+  createAdmin,
+  loginAdmin,
+  adminProfile,
+  adminProfileUpdate,
+} = require("../controllers/admin/admin.auth.controller");
+const role = require("../middlewares/role.middleware");
+const superAdminAuth = require("../middlewares/super.admin.role.middleware");
+
+adminRoute.post(
+  "/create-admin",
+  superAdminAuth,
+  role("super-admin"),
+  adminImgUpload.single("image"),
+  createAdmin
+);
+
+adminRoute.post("/admin-login", loginAdmin);
+
+adminRoute.get(
+  "/admin-profile",
+  adminOuth,
+  role("admin", "super-admin"),
+  adminProfile
+);
+
+adminRoute.put(
+  "/admin-profile-update",
+  adminOuth,
+  role("admin"),
+  adminImgUpload.single("image"),
+  adminProfileUpdate
+);
 
 module.exports = adminRoute;
